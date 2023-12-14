77b22168b10ef5d7d549f6867af4e1e5dc1df348
Sreejith S <ssreejith@protonmail.com>
2023-12-14 13:57:25 -0800

    roman to integer

---
+++ 13_Roman_To_Integer.py
@@ -0,0 +1,31 @@
+
+class Solution:
+
+    def romanToInt(self, s: str) -> int:
+
+        values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000   
+        }
+
+             
+        prev = values[s[0]]
+        res = prev
+        for ch in s[1:]:
+            current = values[ch]
+            if current > prev:
+                res -= prev
+                current -= prev
+
+            res += current            
+            prev = current
+        return res
+
+s = Solution()
+print(s.romanToInt("MCMXCV"))
+
