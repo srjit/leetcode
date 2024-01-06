4f8d3a40d83fc9bfb9e4fed8b07e3d7b78748faf
Sreejith Sreekumar <sreejith2904@gmail.com>
2024-01-06 13:29:35 -0800

    buffer

--- buffer/binary_search.py
+++ buffer/binary_search.py
@@ -6,9 +6,12 @@ def search(arr: List, target: int) -> int :
     low = 0
     high = len(arr)-1
 
+    mid_prev = -9999
+
     while low <= high:
 
         mid = (low+high)//2
+        
         
         if arr[mid] < target:
             low = mid+1
@@ -16,7 +19,10 @@ def search(arr: List, target: int) -> int :
             high = mid
         else:
             return mid
+        
+        mid_prev = mid
 
+    
     return -1
 
 print(search([5,7,7,8,8,10], 8))
