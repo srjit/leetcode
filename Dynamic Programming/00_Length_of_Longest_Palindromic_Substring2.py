0cfa08edd94c5054b34970a4b082fcd756d87aa4
Sreejith S <ssreejith@protonmail.com>
2024-01-06 19:46:41 -0800

    length of longest palindrome

---
+++ Dynamic Programming/00_Length_of_Longest_Palindromic_Substring2.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    def longestPalindrome(self, s: str) -> str:
+        
+        _len = len(s)
+        dp = [[0 for j in range(_len)] for i in range(_len)]
+
+        for k in range(_len):
+            dp[k][k] = 1
+        
+        for j in range(_len):
+            for i in range(_len):
+                if i < j:
+                    if s[i] == s[j]:
+                        dp[i][j] = dp[i+1][j-1]+2
+                    else:
+                        dp[i][j] = max(dp[i][j-1], dp[i+1][j])
+            
+
+        maxlen = dp[0][_len - 1]
+        return maxlen
+
+
+print(Solution().longestPalindrome("babcbab"))
+
+
+
