5fd2faec43e542c5e58d43d970a9977fd5972e9c
Sreejith S <ssreejith@protonmail.com>
2024-04-27 12:50:49 -0700

    course schedule 2

--- Graph/course_schedule_2.py
+++ Graph/course_schedule_2.py
@@ -1,2 +1,33 @@
 
 
+class Solution:
+
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+
+        pre = defaultdict(list)
+        for c, p in prerequisites:
+            pre[c].append(p)
+
+        output = []
+        visit = set()
+        cycle = set()
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visit:
+                return True
+            cycle.add(crs)
+            for p in pre[crs]:
+                if not dfs(p): return False
+            cycle.remove(crs)
+            visit.add(crs)
+            output.append(crs)
+            return True
+
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return []
+
+        return output
+            
